a2ee335512b746e0d703c9e6acea59e12baa19b0
Ewgeni Pasichnyk <e.pasichnyk@campus.lmu.de>
2026-04-22 13:58:40 +0200

    numerik blatt 01 a2

--- ss26/numerik/lnexp-1.py
+++ ss26/numerik/lnexp-1.py
@@ -8,9 +8,10 @@ import numpy as np
 #ax.plot(x, y)
 #plt.show()
 ###
-z = np.linspace(-1e-6, 1e-6, 10000)
+z = np.logspace(-16, 1, 1000)
 w = (np.exp(z)-1)/np.log(np.exp(z))
 
 fig, ax = plt.subplots()
 ax.plot(z, w)
-plt.show()
+#plt.show()
+plt.savefig('bildlnexp1.png')
